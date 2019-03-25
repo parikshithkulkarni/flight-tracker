a44e9efee2fd4bf583aaf4776e178ba712b017a4
Parikshith Kulkarni <parikshith.kulkarni.contractor@bbva.com>
2019-03-25 10:35:07 -0500

    initial heroku commit

---
+++ build/precache-manifest.bfa43ad6bde36755a3570c9d143d2bb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c47b645a70e5415f68e",
+    "url": "/static/css/main.322c58dc.chunk.css"
+  },
+  {
+    "revision": "5c47b645a70e5415f68e",
+    "url": "/static/js/main.1d3a6778.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de31194c524117fac269",
+    "url": "/static/js/2.5b87ddb3.chunk.js"
+  },
+  {
+    "revision": "6312b040d7af8ee31fea5ac50fbd00af",
+    "url": "/index.html"
+  }
+];
